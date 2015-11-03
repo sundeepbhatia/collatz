f56245f25d787c1b425ca09d92aefe7df83f941f
Sundeep Bhatia <sundeepbhatia@hotmail.com>
2015-11-03 00:28:41 -0800

    Scala collatz w/ stop, max & sequence w/ debug

---
+++ src/main/scala/collatz.scala
@@ -0,0 +1,30 @@
+package math.collatz
+
+class Collatz(val debug : Boolean = false) {
+  var max = 0
+  def stop(n : Int) : Int = {
+    if (n > max) max = n
+    if (debug) print(n)
+    if (n == 1) return 1
+    if (debug) print(",")
+    if (n % 2 == 1) {
+      1 + stop(3*n+1)
+    } else {
+      1 + stop(n/2)
+    }
+  }
+}
+
+object Collatz {
+  def main(args : Array[String]) = {
+    val dbg = false
+    for (n <- 1 to 1024) {
+      var collatz = new Collatz(dbg);
+      print("C(" + n + ")")
+      if (dbg) print("=(")
+      var stop = collatz.stop(n)
+      if (dbg) print(")")
+      println("=" + stop + ", max=" + collatz.max)
+    }
+  }
+}
